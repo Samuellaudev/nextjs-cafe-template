cff44dbc0812bda2a9e486dd1739ec58d4e94867
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-01 13:53:07 +0000

    Adjusted the font size, changed background image to video in mobile view to Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,23 +3,23 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section className="relative mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 md:mt-20 md:mb-40">
+    <section className="relative mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 md:mt-10 md:mb-20">
       {/* Background Image */ }
-      <div className="absolute inset-0 bg-cover bg-center bg-no-repeat md:rounded-md" style={ { backgroundImage: "url('/images/hero.webp')" } }></div>
+      <div className="absolute hidden md:block inset-0 bg-cover bg-center bg-no-repeat md:rounded-md" style={ { backgroundImage: "url('/images/hero.webp')" } }></div>
       {/* Content */ }
-      <div className="relative z-10 grid grid-cols-1 gap-4 md:grid-cols-3 md:items-center md:gap-8 border border-gray-300 p-6 rounded-md shadow-md">
+      <div className="relative z-10 grid grid-cols-1 md:grid-cols-3 md:items-center md:gap-8 border border-gray-300 p-6 rounded-md shadow-md">
         <div className='md:col-span-1'>{/* Empty Space */ }</div>
         {/* Left-side - video */ }
         <video
           muted loop autoPlay playsInline
-          className='absolute top-6 left-8 w-[16.8rem] md:w-[18.6rem] h-auto object-cover opacity-90 mx-auto rounded-md shadow-md border-2 border-gray-300'
+          className='absolute top-0 left-0 h-full w-full md:top-6 md:left-8 md:w-[18.6rem] md:h-auto object-cover opacity-90 mx-auto rounded-md shadow-md border-2 border-gray-300'
         >
           <source src="/video/filter_coffee_hero.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
         {/* Right-side - text */ }
-        <div dir="md:rtl" className="relative ml-10 md:ml-0 mt-4 md:mt-0 mb-auto max-w-lg md:max-w-none md:col-span-2 text-white p-8 shadow-md">
+        <div dir="md:rtl" className="relative my-10 md:my-0 max-w-lg md:max-w-none md:col-span-2 text-white p-8 shadow-md">
           <div className='hidden md:block absolute top-1 right-3 w-6/12'>
             <Image
               src='/images/hero_subtitle.png'
@@ -32,11 +32,11 @@ const Hero = () => {
             />
           </div>
           <div className="absolute md:mt-36 inset-0 bg-black/40 md:bg-black/50 rounded-md"></div>
-          <div className="relative md:mt-36 z-10 md:py-10 md:px-4">
-            <h2 className="font-light text-3xl md:text-5xl">
+          <div className="relative md:mt-36 z-10 md:py-10 ">
+            <h2 className="font-light text-[26px] md:text-5xl">
               Experience the Perfect Brew
             </h2>
-            <h2 className="font-light text-3xl md:text-5xl">
+            <h2 className="font-light text-[26px] md:text-5xl">
               Your Daily Coffee Fix
             </h2>
             <p className="font-extralight mt-4">
