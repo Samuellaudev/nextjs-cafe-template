d7e7a7d25b534e65a09e45a4b3a3eecba02f9563
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-01 14:14:41 +0000

    Adjusted the order position in mobile view in Quote

--- components/Quote.jsx
+++ components/Quote.jsx
@@ -8,12 +8,13 @@ const ThinLine = () => (
 
 const Quote = () => {
   return (
-    <section className="relative mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 md:mb-40">
+    <section className="relative mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 md:mb-20">
       {/* Background Image */ }
       <div className="absolute inset-0 bg-cover bg-right md:bg-center bg-no-repeat opacity-70 md:rounded-md" style={ { backgroundImage: "url('/images/quote_bg.png')" } }></div>
       {/* Content */ }
       <div className="relative z-10 container grid grid-cols-1 md:grid-cols-2 gap-4 px-4 py-10 md:py-20 mx-auto text-center text-white border border-gray-300 p-6 rounded-md shadow-md">
-        <div className='relative col-span-1 md:col-span-1 mb-10 md:my-auto mx-4'>
+        {/* Quote */}
+        <div className='relative order-2 md:order-1 col-span-1 md:col-span-1 mt-10  md:my-auto mx-4'>
           <div className="absolute inset-0 bg-gray-300/70 rounded-md shadow-md backdrop-blur-sm"></div>
           <div className="relative py-0 md:py-4">
             <ThinLine />
@@ -25,7 +26,8 @@ const Quote = () => {
             <ThinLine />
           </div>
         </div>
-        <div className='col-span-1 relative'>
+        {/* Image */}
+        <div className='relative order-1 md:order-2 col-span-1'>
           <div className="absolute -top-6 md:-top-10 bottom-10 left-24 md:left-28 inset-0 w-2/3 bg-black/30 rounded-md"></div>
           <Image
             src='/images/barista_1.jpg'
