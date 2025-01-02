0581baaa2bdb20ece7d4af3b90152e335cdaa0eb
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-02 14:47:59 +0000

    Installed react-slick package and photos for "About" page

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { raleway } from '@/components/fonts';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const metadata = {
   title: "Greytone Coffee",
