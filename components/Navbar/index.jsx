0201e91f974100369b1fc28d3e5e4893424130f3
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-03 00:25:48 +0000

    Update z-index of navbar

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -6,7 +6,7 @@ import MobileMenu from './MobileMenu';
 const Navbar = () => {
 
   return (
-    <nav className="fixed mx-auto text-slate-600 top-0 left-0 right-0 z-[20] bg-white bg-opacity-80 backdrop-blur-md backdrop-opacity-80">
+    <nav className="fixed mx-auto text-slate-600 top-0 left-0 right-0 z-[999] bg-white bg-opacity-80 backdrop-blur-md backdrop-opacity-80">
       <div className='mx-auto max-w-7xl px-4 lg:px-8'>
         <div className='relative flex h-20 items-center justify-between'>
           <div className='absolute inset-y-0 right-0 flex items-center md:hidden'>
